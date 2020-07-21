802080c742cb3f2b337d9169a97ae9a101ed3123
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2020-07-21 12:16:05 -0700

    Add files via upload

---
+++ choreList.java
@@ -0,0 +1,15 @@
+
+
+public class choreList<T> {
+
+	public void add(String string) {
+	}
+
+	public void remove(String string) {
+	}
+
+	public int indexOf(String string) {
+		return 0;
+	}
+
+}
---
+++ chores.java
@@ -0,0 +1,23 @@
+class chores {
+ 
+    public static void main(String[] args) {
+
+        // chores list
+        choreList<String> choreList = new choreList<String>();
+
+        choreList.add("Wash the dishes");
+        choreList.add("Clean your room");
+        choreList.add("Dust the house") ;
+        choreList.add("clean the furniture");
+        choreList.add("Make bed");
+
+        choreList.remove("clean the furniture");
+        
+        System.out.println(choreList);
+        // Calculate the chores until the hosue is clean
+        int clean = choreList.indexOf("clean the house");
+
+        // Change the value printed
+        System.out.println(clean);
+    }
+}
